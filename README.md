547bb35fcbf8f5501ba778d1f4f5c650841a56e9
Hidetake Iwata <int128@gmail.com>
2022-02-19 14:04:10 +0900

    Initial commit (#8)
    * Initial commit
    
    * Test format
    
    * Fix
    
    * Fix checkout ref
    
    * Fix
    
    * update-generated-files
    
    * Update README
    
    * Set updated-sha output
    
    * Test format
    
    * update-generated-files
    
    * Update README
    
    * Fix git push
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,21 +1,60 @@
-# typescript-action [![ts](https://github.com/int128/typescript-action/actions/workflows/ts.yaml/badge.svg)](https://github.com/int128/typescript-action/actions/workflows/ts.yaml)
+# update-generated-files-action [![ts](https://github.com/int128/update-generated-files-action/actions/workflows/ts.yaml/badge.svg)](https://github.com/int128/update-generated-files-action/actions/workflows/ts.yaml)
 
-This is a template of GitHub Actions written in TypeScript.
+This is a general-purpose action to update generated files on a pull request.
+
+It synchronizes source and generated files as follows:
+
+- Push a change on pull request event
+- Ensure no change on push event
 
 
 ## Getting Started
 
-To run this action, create a workflow as follows:
+This action is designed to work on a head branch of pull request.
+
+Here is an example.
 
 ```yaml
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+
 jobs:
-  build:
+  generate:
     runs-on: ubuntu-latest
     steps:
-      - uses: int128/typescript-action@v1
+      - uses: actions/checkout@v2
         with:
-          name: hello
+          ref: ${{ github.head_ref }}
+
+      # something to regenerate files
+      - run: yarn
+      - run: yarn generate
+
+      - uses: int128/update-generated-files-action@v2
 ```
+
+### For pull request events
+
+If there is a change (i.e. `git status` returned any change), this action pushes the change to the head branch.
+Otherwise, it does nothing.
+
+You need to explicitly checkout the head branch by setting `ref: ${{ github.head_ref }}`.
+
+### For push or other events
+
+This action creates a pull request to follow up the difference.
+
+
+## Working with Renovate
+
+You can update dependencies using Renovate and this action.
+
+1. Renovate creates a pull request to update a dependency
+1. GitHub Actions triggers a workflow
+1. update-generated-files-action pushes a change if it exists
 
 
 ## Specification
@@ -24,14 +63,17 @@ jobs:
 
 | Name | Default | Description
 |------|----------|------------
-| `name` | (required) | example input
+| `committer-name` | `github-actions[bot]` | A value for git config user.name
+| `committer-email` | see action.yaml | A value for git config user.email
+| `message` | `update-generated-files` | A value for git commit --message
+| `token` | `github.token` | GitHub token
 
 
 ### Outputs
 
 | Name | Description
 |------|------------
-| `example` | example output
+| `updated-sha` | SHA of a new commit if `git push` is done
 
 
 ## Development
--- src/run.ts
+++ src/run.ts
@@ -1,10 +1,80 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as github from '@actions/github'
 
 type Inputs = {
-  name: string
+  comitterName: string
+  comitterEmail: string
+  message: string
+  token: string
 }
 
-// eslint-disable-next-line @typescript-eslint/require-await
 export const run = async (inputs: Inputs): Promise<void> => {
-  core.info(`my name is ${inputs.name}`)
+  if ((await gitStatus()) === '') {
+    core.info('Nothing to commit')
+    return
+  }
+
+  await exec.exec('git', ['config', 'user.name', inputs.comitterName])
+  await exec.exec('git', ['config', 'user.email', inputs.comitterEmail])
+
+  if (github.context.eventName !== 'pull_request') {
+    core.info(`Creating a pull request to follow up the difference`)
+    await createFollowUpPullRequest(inputs)
+    throw new Error(`${github.context.ref} is broken because there is difference between source and generated files`)
+  }
+
+  await exec.exec('git', ['add', '.'])
+  await exec.exec('git', ['status'])
+  await exec.exec('git', ['commit', '-m', inputs.message])
+  await exec.exec('git', ['push'])
+
+  const headOutput = await exec.getExecOutput('git', ['rev-parse', 'HEAD'])
+  const headSHA = headOutput.stdout.trim()
+  core.info(`head SHA is ${headSHA}`)
+  core.setOutput('updated-sha', headSHA)
+}
+
+const createFollowUpPullRequest = async (inputs: Inputs) => {
+  const [, , base] = github.context.ref.split('/')
+  const head = `update-generated-files-${github.context.sha}-${github.context.runNumber}`
+  const body = `Hi ${github.context.actor},
+${base} branch is broken because there is difference between source and generated files at ${github.context.sha}.
+This pull request will fix the difference.
+`
+
+  await exec.exec('git', ['checkout', '-b', head])
+  await exec.exec('git', ['add', '.'])
+  await exec.exec('git', ['status'])
+  await exec.exec('git', ['commit', '-m', inputs.message])
+  await exec.exec('git', ['push', 'origin', head])
+
+  const octokit = github.getOctokit(inputs.token)
+  const { data: pull } = await octokit.rest.pulls.create({
+    ...github.context.repo,
+    base,
+    head,
+    title: 'Update generated files',
+    body,
+  })
+  core.info(`Created ${pull.html_url}`)
+
+  core.info(`Requesting a review to ${github.context.actor}`)
+  await octokit.rest.pulls.requestReviewers({
+    ...github.context.repo,
+    pull_number: pull.number,
+    reviewers: [github.context.actor],
+  })
+
+  core.info(`Adding ${github.context.actor} to assignees`)
+  await octokit.rest.issues.addAssignees({
+    ...github.context.repo,
+    issue_number: pull.number,
+    assignees: [github.context.actor],
+  })
+}
+
+const gitStatus = async (): Promise<string> => {
+  const o = await exec.getExecOutput('git', ['status', '--porcelain'])
+  return o.stdout.trim()
 }
