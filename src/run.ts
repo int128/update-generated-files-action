cd2b783a58fdbcbe729f30d7eb48a9b03c185906
Hidetake Iwata <int128@gmail.com>
2022-02-19 12:25:09 +0900

    Fix

--- src/run.ts
+++ src/run.ts
@@ -28,7 +28,7 @@ export const run = async (inputs: Inputs): Promise<void> => {
   await exec.exec('git', ['add', '.'])
   await exec.exec('git', ['status'])
   await exec.exec('git', ['commit', '-m', inputs.message])
-  await exec.exec('git', ['push', 'origin', `HEAD:${github.context.ref}`])
+  await exec.exec('git', ['push', 'origin'])
 }
 
 const createFollowUpPullRequest = async (inputs: Inputs) => {
