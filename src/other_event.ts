54f90d248037d2fef9cc334300d79b8a46920e80
Hidetake Iwata <int128@gmail.com>
2023-08-05 17:39:46 +0900

    Fix

--- src/other_event.ts
+++ src/other_event.ts
@@ -5,6 +5,8 @@ import { Inputs } from './run'
 import { Context } from '@actions/github/lib/context'
 import { RequestError } from '@octokit/request-error'
 
+const LIMIT_REPEATED_COMMITS = 5
+
 export type PartialContext = Pick<Context, 'ref' | 'repo' | 'actor' | 'sha' | 'runNumber' | 'eventName'>
 
 export const handleOtherEvent = async (inputs: Inputs, context: PartialContext) => {
@@ -12,34 +14,37 @@ export const handleOtherEvent = async (inputs: Inputs, context: PartialContext) 
     core.warning('This action handles only branch event')
     return
   }
-  if (inputs.followUpMethod === 'fast-forward-or-pull-request') {
-    return fastForwardOrPull(inputs, context)
+
+  await git.commit(`${inputs.commitMessage}\n\n${inputs.commitMessageFooter}`)
+
+  // do not change the current HEAD after here
+  core.info(`Trying to update ${context.ref} by fast-forward`)
+  if (await updateRefByFastForward(inputs, context)) {
+    return
   }
-  if (inputs.followUpMethod === 'fast-forward') {
-    return fastForward(inputs, context)
-  }
-  if (inputs.followUpMethod === 'pull-request') {
-    return createPull(inputs, context)
-  }
-  throw new Error(`unknown follow-up-method: ${inputs.followUpMethod}`)
+  core.info(`Falling back to create a pull request to follow up`)
+  return await createPull(inputs, context)
 }
 
-const fastForwardOrPull = async (inputs: Inputs, context: PartialContext) => {
-  try {
-    await fastForward(inputs, context)
-  } catch (e) {
-    await createPull(inputs, context)
+const updateRefByFastForward = async (inputs: Inputs, context: PartialContext) => {
+  core.info(`Checking the last commits to prevent infinite loop`)
+  await git.fetch({ refs: [context.sha], depth: LIMIT_REPEATED_COMMITS, token: inputs.token })
+  const lastAuthorNames = await git.getAuthorNameOfCommits(context.sha, LIMIT_REPEATED_COMMITS)
+  if (lastAuthorNames.every((authorName) => authorName == git.AUTHOR_NAME)) {
+    core.error(`This action has been called ${LIMIT_REPEATED_COMMITS} times. It may be infinite loop`)
+    return false
   }
+
+  const code = await git.push({ ref: context.ref, token: inputs.token, ignoreReturnCode: true })
+  return code === 0
 }
 
 const createPull = async (inputs: Inputs, context: PartialContext) => {
-  const octokit = github.getOctokit(inputs.token)
-
   const head = `update-generated-files-${context.sha}-${context.runNumber}`
   core.info(`Creating a new branch ${head}`)
-  await git.commit(`${inputs.commitMessage}\n\n${inputs.commitMessageFooter}`)
   await git.push({ ref: `refs/heads/${head}`, token: inputs.token })
 
+  const octokit = github.getOctokit(inputs.token)
   const base = context.ref.replace(/^refs\/heads\//, '')
   core.info(`Creating a pull request for ${base} branch`)
   const { data: pull } = await octokit.rest.pulls.create({
@@ -111,21 +116,4 @@ const catchRequestError = async <T, U>(f: () => Promise<T>, g: (error: RequestEr
     }
     throw error
   }
-}
-
-const LIMIT_REPEATED_COMMITS = 5
-
-const fastForward = async (inputs: Inputs, context: PartialContext) => {
-  core.info(`Updating ${context.ref} by fast-forward`)
-  await git.fetch({ refs: [context.sha], depth: LIMIT_REPEATED_COMMITS, token: inputs.token })
-  const lastAuthorNames = await git.getAuthorNameOfCommits(context.sha, LIMIT_REPEATED_COMMITS)
-  if (lastAuthorNames.every((authorName) => authorName == git.AUTHOR_NAME)) {
-    throw new Error(
-      `This action has been called ${LIMIT_REPEATED_COMMITS} times. Stop the job to prevent infinite loop.`,
-    )
-  }
-
-  await git.commit(`${inputs.commitMessage}\n\n${inputs.commitMessageFooter}`)
-  await git.push({ ref: context.ref, token: inputs.token })
-  core.info(`Updated ${context.ref} by fast-forward`)
 }
