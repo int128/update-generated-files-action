1843a72397ff282c16ed85e781a048d84574ab2d
Hidetake Iwata <int128@gmail.com>
2025-10-26 16:26:55 +0900

    Fetch before cherry-pick

--- src/pull_request_event.ts
+++ src/pull_request_event.ts
@@ -26,6 +26,7 @@ export const handlePullRequestEvent = async (inputs: Inputs, context: Context<Pu
   if (checkoutSHA === context.sha) {
     await updateHeadRefBasedOnMergeCommit(inputs, context)
   } else {
+    await git.commit(`${inputs.commitMessage}\n\n${inputs.commitMessageFooter}`)
     const headRef = context.payload.pull_request.head.ref
     core.info(`Updating the head branch ${headRef}`)
     await git.showGraph()
@@ -42,11 +43,25 @@ export const handlePullRequestEvent = async (inputs: Inputs, context: Context<Pu
 }
 
 const updateHeadRefBasedOnMergeCommit = async (inputs: Inputs, context: Context<PullRequestEvent>) => {
+  const parentSHAs = await git.getParentSHAs(context.sha)
   const headSHA = context.payload.pull_request.head.sha
-  const headRef = context.payload.pull_request.head.ref
+  const baseSHA = parentSHAs.filter((sha) => sha !== headSHA).pop()
+  if (baseSHA === undefined) {
+    throw new Error(`could not determine base commit from parents ${String(parentSHAs)}`)
+  }
+  for (let depth = 50; depth < 1000; depth += 50) {
+    if (await git.canMerge(baseSHA, headSHA)) {
+      break
+    }
+    core.info(`Fetching more commits (depth ${depth})`)
+    await git.fetch({ refs: [baseSHA, headSHA], depth, token: inputs.token })
+  }
 
   await git.commit(`${inputs.commitMessage}\n\n${inputs.commitMessageFooter}`)
   const workspaceChangeSHA = await git.getCurrentSHA()
+
+  const headRef = context.payload.pull_request.head.ref
+  const baseRef = context.payload.pull_request.base.ref
 
   await git.checkout(headSHA)
   if (await git.tryCherryPick(workspaceChangeSHA)) {
@@ -62,36 +77,6 @@ const updateHeadRefBasedOnMergeCommit = async (inputs: Inputs, context: Context<
   // https://github.com/int128/update-generated-files-action/issues/351
   core.info(`Re-merging base branch into head branch`)
   await git.checkout(headSHA)
-  await recreateMergeCommit(inputs, context)
-  await git.tryCherryPick(workspaceChangeSHA)
-  core.info(`Updating the head branch ${headRef}`)
-  await git.showGraph()
-  await git.push({ ref: `refs/heads/${headRef}`, token: inputs.token })
-}
-
-const recreateMergeCommit = async (inputs: Inputs, context: Context<PullRequestEvent>) => {
-  const parentSHAs = await git.getParentSHAs(context.sha)
-  const headSHA = context.payload.pull_request.head.sha
-  const baseSHA = parentSHAs.filter((sha) => sha !== headSHA).pop()
-  if (baseSHA === undefined) {
-    core.warning(`Could not determine base commit from parents ${String(parentSHAs)}`)
-    return
-  }
-  const headRef = context.payload.pull_request.head.ref
-  const baseRef = context.payload.pull_request.base.ref
-  core.info(`head: ${headSHA} ${headRef}`)
-  core.info(`base: ${baseSHA} ${baseRef}`)
-
-  for (let depth = 50; depth < 1000; depth += 50) {
-    core.info(`Fetching more commits (depth ${depth})`)
-    await git.fetch({ refs: [baseSHA, headSHA], depth, token: inputs.token })
-    if (await git.canMerge(baseSHA, headSHA)) {
-      break
-    }
-  }
-
-  await git.showGraph()
-  await git.checkout(headSHA)
   await git.merge(
     baseSHA,
     `Merge branch '${baseRef}' into ${headRef}
@@ -99,4 +84,8 @@ const recreateMergeCommit = async (inputs: Inputs, context: Context<PullRequestE
 Updated the head branch since the current workflow is running on the merge commit.
 ${inputs.commitMessageFooter}`,
   )
+  await git.tryCherryPick(workspaceChangeSHA)
+  core.info(`Updating the head branch ${headRef}`)
+  await git.showGraph()
+  await git.push({ ref: `refs/heads/${headRef}`, token: inputs.token })
 }
