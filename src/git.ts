4b7ce264d9a2479ed206939ef597d7b30223d777
Hidetake Iwata <int128@gmail.com>
2025-10-26 16:12:14 +0900

    Revert

--- src/git.ts
+++ src/git.ts
@@ -49,9 +49,7 @@ export const canMerge = async (base: string, head: string): Promise<boolean> =>
   (await exec.exec('git', ['merge-base', base, head], { ignoreReturnCode: true })) === 0
 
 export const tryCherryPick = async (sha: string): Promise<boolean> => {
-  const code = await exec.exec('git', ['-c', 'advice.mergeConflict=false', 'cherry-pick', sha], {
-    ignoreReturnCode: true,
-  })
+  const code = await exec.exec('git', ['cherry-pick', sha], { ignoreReturnCode: true })
   if (code === 0) {
     return true
   }
